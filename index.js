84719f13964df44cd5f77bf1cd2dd641116ffab3
san <florescencebit@gmail,com>
2025-05-20 10:54:54 +0530

    version 2

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 document.write("hello world!");
-document.body.style.background = "red"
+document.body.style.background = "red";
+document.body.style.textAlign = "center"
+document.body.style.color = "white"
